8fad262c8110b5adf85ed21d42ac0c4846811983
Anatoly <kp_anat@mail.ru>
2023-12-15 09:28:48 +0300

    Some corrections

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]){
 
   std::cout << "OpenGL " << GLVersion.major << '.' << GLVersion.minor << std::endl; 
   
-  glClearColor(0, 1, 0, 1);
+  glClearColor(1, 1, 0, 1);
   
   
   /* Loop until the user closes the window */
